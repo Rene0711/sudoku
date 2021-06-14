9c26a7b819571f2d1b116ff019475dc31ba57c66
Marvins13 <inf18005@lehre.dhbw-stuttgart.de>
2021-06-14 15:08:41 +0200

    remove

--- sudoku/templatetags/sudoku_extras.py
+++ sudoku/templatetags/sudoku_extras.py
@@ -8,7 +8,6 @@ def letter(values, l):
 
 @register.filter(name='number')
 def number(values_l, n):
-    """Finds the value in a dictionary"""
     values, l = values_l
     if values:
         return values[l + n]
